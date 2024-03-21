6228a0ce5c1482e03dc13f14a6f2119f952255b6
Smalljihoon <jihoon4578@gmail.com>
2024-03-22 00:58:57 +0900

    초기 수정
    보드, UI등 이미지, 위치 수정

---
+++ Assets/Script/Enemy.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.Tilemaps;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] GameObject enemy;
+
+    [SerializeField] float speed;
+    RaycastHit ray;
+    private void Start()
+    {
+        
+    }
+    void Update()
+    {
+        enemy = MoveTowards()
+    }
+
+    
+
+}
