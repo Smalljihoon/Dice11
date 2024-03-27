4e48e681d2f46d1dad75f5c036e26c25667aedc1
Smalljihoon <jihoon4578@gmail.com>
2024-03-28 02:50:59 +0900

    커밋 1-4
    적 움직임(현재 위로만 움직임)
    스포너 만들어서 적 소환
    구현

--- Assets/Script/Enemy.cs
+++ Assets/Script/Enemy.cs
@@ -7,18 +7,37 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject enemy;
-
     [SerializeField] float speed;
-    RaycastHit ray;
+    [SerializeField] bool isTurn;
+
+    private Rigidbody2D rigid;
+    private BoxCollider2D boxCollider;
+    private RaycastHit rayHit;
+
     private void Start()
     {
-        
-    }
-    void Update()
-    {
-        //enemy = MoveTowards()
+        rigid = GetComponent<Rigidbody2D>();
     }
 
-    
+    private void FixedUpdate()
+    {
+        Debug.DrawRay(rigid.position, Vector2.up, new Color(1, 0, 0));
+
+    }
+
+    void Update()
+    {
+        transform.Translate(Vector2.up * speed * Time.deltaTime);
+
+    }
+
+    void Raycast()
+    {
+        if (isTurn)
+            this.transform.Rotate(90, 0, 0);
+
+        Vector3 point = transform.position + (Vector3.up * boxCollider.size.y);
+
+    }
 
 }
---
+++ Assets/Script/Spawn.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class Spawn : MonoBehaviour
+{
+
+    [Header("Object")]
+    [SerializeField] GameObject enemyPrefab;
+
+    [Header("Transform")]
+    [SerializeField] Transform StartSpawn;
+    [SerializeField] Transform EndSpawn;
+
+    [Header("Position")]
+    [SerializeField] float spawnRate;               // 스폰주기
+    [SerializeField] float offsetRate;              // 스폰주기 차이
+
+    bool isSpawn;
+    float rateTime;
+
+    void Start()
+    {
+        rateTime = spawnRate;
+        StartSpawn = GetComponent<Transform>();
+        EndSpawn = GetComponent<Transform>();
+
+    }
+
+    void Update()
+    {
+        //if (!isSpawn)
+            //return;
+        
+        rateTime -= Time.deltaTime;
+        if (rateTime <= 0.0f)
+        {
+            //Vector2 position = StartSpawn.position;
+            Transform transform = StartSpawn.transform;
+            GameObject newEnemy = enemyPrefab;
+            newEnemy = Instantiate(enemyPrefab);
+            newEnemy.transform.position = transform.position;
+            //newEnemy.transform.position = position;
+            rateTime = spawnRate;
+        }
+    }
+}
