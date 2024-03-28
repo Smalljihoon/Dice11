45ea83b4ffe65ae7dadf6dd818fb86c56d5748f7
Smalljihoon <jihoon4578@gmail.com>
2024-03-28 15:38:24 +0900

    초기 커밋 1 - 5
    적 움직임 첫번째 벽에서 충돌을 이용하여 회전

--- Assets/Script/Enemy.cs
+++ Assets/Script/Enemy.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using UnityEditor;
 using UnityEditor.Tilemaps;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject enemy;
+    [SerializeField] Transform enemyTransform;
     [SerializeField] float speed;
-    [SerializeField] bool isTurn;
+    //[SerializeField] bool isTurn;
+    [SerializeField] float distance;
 
     private Rigidbody2D rigid;
     private BoxCollider2D boxCollider;
-    private RaycastHit rayHit;
+    private RaycastHit2D hit;
 
     private void Start()
     {
@@ -21,23 +24,38 @@ public class Enemy : MonoBehaviour
 
     private void FixedUpdate()
     {
-        Debug.DrawRay(rigid.position, Vector2.up, new Color(1, 0, 0));
+        var pos = enemy.transform.position;
 
+        Debug.DrawRay(pos, enemy.transform.up, new Color(1, 0, 0), LayerMask.GetMask("Wall"));
+
+        hit = Physics2D.Raycast(pos, Vector2.up, distance, LayerMask.GetMask("Wall"));
+
+            if (hit.collider != null)
+            {
+
+                Debug.Log(hit.collider.name);
+            enemy.transform.localEulerAngles = new Vector3(0, 0, -90);
+            //Debug.Log(enemy.transform.rotation.eulerAngles);
+        }
+        
     }
 
     void Update()
     {
+
         transform.Translate(Vector2.up * speed * Time.deltaTime);
 
     }
 
-    void Raycast()
-    {
-        if (isTurn)
-            this.transform.Rotate(90, 0, 0);
+    private void OnCollisionEnter2D(Collision2D collision)
 
-        Vector3 point = transform.position + (Vector3.up * boxCollider.size.y);
+    {
+
+        if (collision.gameObject.tag == "Wall")
+
+        {
+
+        }
 
     }
-
 }
