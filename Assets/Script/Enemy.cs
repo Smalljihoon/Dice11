28cd130a932dfbccf6d203bb2e5e6a74f52f4630
Smalljihoon <jihoon4578@gmail.com>
2024-03-29 15:42:40 +0900

    초기 커밋 1 - 6
    enemy 스크립트 rotationZ변수 추가
    두번째 벽 인식은 하나 방향을 꺾지는 않는 문제 (해결중)

--- Assets/Script/Enemy.cs
+++ Assets/Script/Enemy.cs
@@ -8,14 +8,14 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject enemy;
-    [SerializeField] Transform enemyTransform;
+    //[SerializeField] Transform enemyTransform;
     [SerializeField] float speed;
     //[SerializeField] bool isTurn;
     [SerializeField] float distance;
 
     private Rigidbody2D rigid;
     private BoxCollider2D boxCollider;
-    private RaycastHit2D hit;
+    //private RaycastHit2D hit;
 
     private void Start()
     {
@@ -26,36 +26,38 @@ public class Enemy : MonoBehaviour
     {
         var pos = enemy.transform.position;
 
-        Debug.DrawRay(pos, enemy.transform.up, new Color(1, 0, 0), LayerMask.GetMask("Wall"));
+        Debug.DrawRay(pos, enemy.transform.up, new Color(1, 0, 0));
 
-        hit = Physics2D.Raycast(pos, Vector2.up, distance, LayerMask.GetMask("Wall"));
+        var hit = Physics2D.Raycast(pos, Vector2.up, distance, LayerMask.GetMask("Wall"));
 
-            if (hit.collider != null)
-            {
-
-                Debug.Log(hit.collider.name);
+        if (hit.collider != null)
+        {
+            float rotationZ = enemy.transform.localEulerAngles.z + -90f;
+            //Debug.Log(hit.collider.name);
             enemy.transform.localEulerAngles = new Vector3(0, 0, -90);
+            Debug.Log(rotationZ);
+            
             //Debug.Log(enemy.transform.rotation.eulerAngles);
         }
-        
+
     }
 
     void Update()
     {
 
-        transform.Translate(Vector2.up * speed * Time.deltaTime);
+        enemy.transform.Translate(Vector2.up * speed * Time.deltaTime);
 
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    //private void OnCollisionEnter2D(Collision2D collision)
 
-    {
+    //{
 
-        if (collision.gameObject.tag == "Wall")
+    //    if (collision.gameObject.tag == "Wall")
 
-        {
+    //    {
 
-        }
+    //    }
 
-    }
+    //}
 }
