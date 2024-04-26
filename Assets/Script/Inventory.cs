9a5a1e46a69ea2fd851a304ec844e39bf3b6f119
Smalljihoon <jihoon4578@gmail.com>
2024-04-26 15:50:32 +0900

    커밋 3-7
    스타트 씬
    ui배치

--- Assets/Script/Inventory.cs
+++ Assets/Script/Inventory.cs
@@ -6,9 +6,14 @@ using UnityEngine.UI;
 public class Inventory : MonoBehaviour
 {
     [SerializeField] Button inventorybt;
-    [SerializeField] Button battlebt;
-    [SerializeField] Button exitbt;
+    [SerializeField] Button invenExitbt;
+
+    [SerializeField] Button[] choicebt;
+
     [SerializeField] GameObject invenpanel;
+    
+    // 주사위 선택 버튼을 누르면 해당 버튼의 주사위 이미지, 설명 등이 나온다
+    // 선택, 나가기 버튼이 있는데 선택을 누르면 나의 덱에 추가를 한다
 
     private void Start()
     {
